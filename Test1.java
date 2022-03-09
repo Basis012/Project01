e9bce7eaf4fd3e71ae148b7ed3f92e71fe92d6d8
Saqib <101037188+Basis012@users.noreply.github.com>
2022-03-09 14:59:03 +0500

    This is my first project

---
+++ Test1.java
@@ -0,0 +1,14 @@
+package project1;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Declaring two variables
+		int x = 20;
+		int y = 30;
+		int Result = x + y;
+		System.out.println("Two number sum is :" + Result);
+	}
+
+}
